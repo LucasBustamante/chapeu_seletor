479f76171281a0bfef836c0ae88c67bd864c67dc
LucasBustamante <lucascostabustamante@gmail.com>
2022-03-22 00:45:40 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/chapeu_seletor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chapeu_seletor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Stack(
+                children: [
+              Container(child: Image.asset('hogwarts.jpg'),),
+                Container(child: Image.asset('hagrid.png'),)]),
+          )
+        ],
+      ),
+    );
+  }
+}
+
